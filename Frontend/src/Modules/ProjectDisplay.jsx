0afae42f9a0fe7b1b0f0d6b552105dc5e2e0fd4e
Jimil Soni <jimilsoni926@gmail.com>
2025-03-30 20:30:10 +0530

    Added github repo link to the fourth project

--- Frontend/src/Modules/ProjectDisplay.jsx
+++ Frontend/src/Modules/ProjectDisplay.jsx
@@ -172,7 +172,7 @@ const ProjectDisplay = () => {
           </ul>
         </div>
 
-        <a href="https://github.com/jimil222/AI-trip-Planner" className="order-1 sm:order-2 flex w-full relative justify-center sm:justify-start" target='_blank'>
+        <a href="https://github.com/jimil222/AI-Writer-Studio" className="order-1 sm:order-2 flex w-full relative justify-center sm:justify-start" target='_blank'>
           <div className="flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200 sm:ml-auto">
             <img
               className="max-w-[400px] w-full relative z-10 drop-shadow-[0_0px_60px_rgba(66,198,161)]"
